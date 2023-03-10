8d9bfd6510099ca97215cbc657c445dea73a5588
Alavanh Chanthasaly <ayedawut@gmail.com>
2023-03-09 19:06:55 -0500

    sketching out master script

---
+++ AL-DEV-Master.ps1
@@ -0,0 +1,7 @@
+# This is the Master script that will connect all the others.
+
+#TODO - Making a prompt that lets us choose a script to run.
+Write-host -Prompt "Please choose an option below"
+$ObjectList = @('Object 1', 'Object 2', 'Object 3', 'Object 4')
+# TODO -Choose a specific object in the above array.
+# This way, we can have the user select a script to run
